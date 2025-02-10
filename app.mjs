2efa68d9469055051c008366fd0cada5c855c58a
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2025-02-11 01:35:10 +0700

    fix: ui docs and some small changes

--- app.mjs
+++ app.mjs
@@ -9,12 +9,15 @@ import authenticateRoutes from "./routes/authenticateRoutes.mjs";
 import { logMiddleware, logStream } from "./middlewares/logMiddleware.mjs";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
-import { swaggerDocs } from "./docs/swaggerDef.mjs";
+import { swaggerDocs, swaggerUiOptions } from "./docs/swaggerDef.mjs";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs, swaggerUiOptions));
+console.log(`API Docs available at http://localhost:${PORT}/docs`);
 
 
 app.use(logMiddleware);
@@ -22,9 +25,6 @@ app.use(cors());
 
 app.use("/uploads", express.static("uploads"));
 
-// SWAGGER
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-console.log(`API Docs available at http://localhost:${PORT}/api-docs`);
 
 app.use(express.json());
 
--- docs/swaggerDef.mjs
+++ docs/swaggerDef.mjs
@@ -15,7 +15,7 @@ const swaggerOptions = {
       title: "E-commerce API",
       version: "1.0.11",
       description:
-        "an API for an e-commerce website build with Express.js by memoowi",
+        "Built with Express.js by memoowi, this demo e-commerce API offers a practical example of how to build an online store.  It covers essential features like product, user, category, review, cart, etc.",
       contact: {
         name: "memoowi",
         email: "megatenlike@gmail.com",
@@ -65,7 +65,7 @@ const swaggerOptions = {
       ...addressDocs,
       ...categoryDocs,
       ...productDocs,
-      ...adminDocs
+      ...adminDocs,
     },
   },
   apis: ["./routes/*.mjs"],
@@ -73,3 +73,19 @@ const swaggerOptions = {
 };
 
 export const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+export const swaggerUiOptions = {
+  customCss: `
+    body { background-color:rgb(232, 242, 255) !important; }
+    .topbar { display: none; }
+    .swagger-ui .information-container { padding: 0 !important; max-width: 100% !important; }
+    .swagger-ui .info { background-color:rgb(0, 11, 81) !important; padding: 40px; color: white !important; text-align: center; margin: 0; }
+    .swagger-ui .info .title { color:rgb(95, 149, 255) !important; font-weight: bold; }
+    .swagger-ui .info .description p { color:rgb(255, 255, 255) !important; font-weight: 500; font-size: 16px; max-width: 640px !important; }
+    .link { color:rgb(201, 143, 255) !important; font-size: 16px; }
+    .link:hover { color:rgb(255, 251, 192) !important; text-decoration: underline }
+    .swagger-ui .btn { background-color:rgb(29, 20, 147) !important; color: white !important; }
+  `,
+  customSiteTitle: "E-comm API by Memoowi", // Custom Page Title
+  customfavIcon: "https://example.com/favicon.ico", // Custom Favicon
+};
